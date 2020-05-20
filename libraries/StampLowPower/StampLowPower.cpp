37ebe5935c96c638676dde92623d3e25234da63d
Daniel de Kock <daniel@riot.network>
2020-05-20 16:26:48 +0200

    Update StampLowPower.cpp
    Slowed down CPU and peripheral clocks during sleep

--- libraries/StampLowPower/StampLowPower.cpp
+++ libraries/StampLowPower/StampLowPower.cpp
@@ -59,7 +59,7 @@ void StampLowPower::sleep() {
 	bool restoreUSBDevice = false;
 	if (SERIAL_PORT_USBVIRTUAL) {
 		USBDevice.standby();
-	} else {
+		} else {
 		USBDevice.detach();
 		restoreUSBDevice = true;
 	}
@@ -73,7 +73,7 @@ void StampLowPower::sleep() {
 	while (GCLK->SYNCBUSY.reg & GCLK_SYNCBUSY_GENCTRL(0)) {
 		/* Wait for synchronization */
 	};
- 	OSCCTRL->OSC16MCTRL.reg |= OSCCTRL_OSC16MCTRL_ONDEMAND;
+	OSCCTRL->OSC16MCTRL.reg |= OSCCTRL_OSC16MCTRL_ONDEMAND;
 
 	/* Clear performance level status */
 	PM->INTFLAG.reg = PM_INTFLAG_PLRDY;
@@ -90,27 +90,41 @@ void StampLowPower::sleep() {
 	OSCCTRL->DFLLCTRL.reg = dfllCtrlSlp.reg;
 
 	// disable DFLL GCLK
-	/* Disable the peripheral channel */
+	/* Disable the peripheral channel 0 ( DFLL )*/
 	GCLK->PCHCTRL[0].reg &= ~GCLK_PCHCTRL_CHEN;
 
 	while (GCLK->PCHCTRL[0].reg & GCLK_PCHCTRL_CHEN) {
 		/* Wait for clock synchronization */
 	}
+	
 
-	// disable xosc32k clock 
- 	OSC32KCTRL->XOSC32K.reg &= ~OSC32KCTRL_XOSC32K_ENABLE;
+	// disable xosc32k clock
+	OSC32KCTRL->XOSC32K.reg &= ~OSC32KCTRL_XOSC32K_ENABLE;
 
 	// disable generator 1
 	GCLK->GENCTRL[1].bit.GENEN = 0;
 	
 	// Disable systick interrupt:  See https://www.avrfreaks.net/forum/samd21-samd21e16b-sporadically-locks-and-does-not-wake-standby-sleep-mode
-	SysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk;	
+	SysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk;
+	// set core voltage regulator to "runstandby" - erratta Main Voltage Regulator Reference:15264
+	//SUPC->VREG.bit.RUNSTDBY = 1;
+	//SUPC->VREG.bit.STDBYPL0 = 1;
+	
+	/* CPU and BUS clocks slow down - slow down busses BEFORE cpu.. */
+	MCLK->BUPDIV.reg = MCLK_BUPDIV_BUPDIV_DIV128;/** Divide Main clock ( 4MHz OSC ) by 64,ie run at 31.768kHz */
+	MCLK->LPDIV.reg = MCLK_BUPDIV_BUPDIV_DIV128; /** Divide low power clock ( 4MHz OSC ) by 64, ie run at 31.768kHz*/
+	MCLK->CPUDIV.reg = MCLK_CPUDIV_CPUDIV_DIV64; /**(MCLK_CPUDIV) Divide by 64 ,ie run at 62.5kHz */
 	
 	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
 	__DSB();
 	__WFI();
 	// sleeping here, will wake from here ( except from OFF or Backup modes, those look like POR )
-
+	
+	/* CPU and BUS clocks back to "regular ratio"*/
+	MCLK->CPUDIV.reg = MCLK_CPUDIV_CPUDIV_DIV1; /**(MCLK_CPUDIV) Divide by 1 ,ie run at 4MHz.. until we start the DFLL again */
+	MCLK->BUPDIV.reg = MCLK_BUPDIV_BUPDIV_DIV1;/** Div 1, so run these at main clock rate */
+	MCLK->LPDIV.reg = MCLK_BUPDIV_BUPDIV_DIV1; /**low power domain back to CPU clock speed */
+	
 	// enable xosc32k clock
 	OSC32KCTRL->XOSC32K.reg |= OSC32KCTRL_XOSC32K_ENABLE;
 	// wait for clock to become ready
@@ -150,16 +164,15 @@ void StampLowPower::sleep() {
 	while (GCLK->SYNCBUSY.reg & GCLK_SYNCBUSY_GENCTRL(0)) {
 		/* Wait for synchronization */
 	};
-//	GCLK->GENCTRL[0].reg |= GCLK_GENCTRL_GENEN;
+	//	GCLK->GENCTRL[0].reg |= GCLK_GENCTRL_GENEN;
 	/*  Switch to PL2 to be sure configuration of GCLK0 is safe */
 	// Enable systick interrupt
 	SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
 	
-	
 	if (restoreUSBDevice) {
 		delay(1);
 		USBDevice.init();
-		USBDevice.attach();	
+		USBDevice.attach();
 	}
 	
 }
